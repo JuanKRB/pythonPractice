7124372dcc1932d53fe431c04ee653a7d3aea8fe
JuanKRB <juanca.rodriguez.bon@gmail.com>
2024-06-29 18:18:41 -0600

    Initial commit

---
+++ Lists.py
@@ -0,0 +1,28 @@
+list2 = [2,3,"Hi"]
+
+print(list2[-1])
+
+print(list2[0:2])
+
+a = [1, 'a']
+
+b = [2, 1, 'd']
+
+a.append(b)
+
+print(a)
+
+b.extend(a)
+
+print(b)
+
+print(len(b))
+
+print(b[-6])
+
+del(b[-1])
+
+print(b)
+
+print(type(b))
+
---
+++ Tuples.py
@@ -0,0 +1,30 @@
+tuple1 = (1,2,"hello")
+
+print(type(tuple1))
+
+print(tuple1[-1])
+
+tuple1 = tuple1 + ("hard rock", 10)
+print(tuple1)
+
+print(tuple1[0:4])
+
+print(len(tuple1))
+
+
+Ratings = (0, 9, 6, 5, 10, 8, 9, 6, 2)
+RatingsSorted = sorted(Ratings)
+print(RatingsSorted)
+
+
+NestedT = (1, 2, ("pop", "rock") ,(3,4),("disco",(1,2)))
+
+print(NestedT[2][1])
+
+print(NestedT[-1][1][-2])
+
+print("disco".find('s'))
+
+
+tuplet5 = (1,2,3,4)
+print(len(tuplet5))
